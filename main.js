2472a6e699c38943ff27433cb4b65e212a7b4740
JeffreyAlan699 <78485523+JeffreyAlan699@users.noreply.github.com>
2021-04-18 13:03:25 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,23 +1,24 @@
-var canvas = new fabric.Canvas("myCanvas");
+var canvas = new fabric.Canvas("myCanvas")
 var block_image_width = 30;
 var block_image_height = 30;
 var player_y = 10;
 var player_x = 10;
-var block_image_object = "";
 var player_object = "";
+var block_image_object ="";
 function player_update(){
-    fabric.Image.fromURL('superhero.png',function(Img){
+    fabric.Image.fromURL("player.png",function(Img){
         player_object = Img;
         player_object.scaleToWidth(150);
         player_object.scaleToHeight(140);
-        player_object.set({ top:player_y, // Y
+        player_object.set({ 
+        top:player_y, // Y
         left:player_x // X
          });
         canvas.add(player_object);
-    })
+    });
 }
 function new_image(get_image){
-    fabric.Image.fromURL('get_image',function(Img){
+    fabric.Image.fromURL(get_image,function(Img){
         block_image_object = Img;
         block_image_object.scaleToWidth(block_image_width);
         block_image_object.scaleToHeight(block_image_height);
@@ -27,4 +28,12 @@ function new_image(get_image){
          });
         canvas.add(block_image_object);
     });
+}
+window.addEventListener("keydown",my_keydown);
+function my_keydown(e){
+    var keypressed = e.keyCode;
+    if(keypressed == "80"){
+        new_image("player.png");
+        console.log("p");
+    }
 }
